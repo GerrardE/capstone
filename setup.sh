1de1147dcb904a19ab5cf5eab5fa832f661d685c
gerrard.ezeugwa <gerrard.ezeugwa@carterltd.com>
2020-02-16 17:25:09 +0100

    Create migration files

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
 export FLASK_APP='app'
 export FLASK_ENV='production'
 export FLASK_DEBUG=True
-DATABASE_URL="postgres://afowwmgwurwngb:305bde8039ad604c7fdc449576461feb7d56387e1b71aed07497989b1c16ef9a@ec2-184-72-235-159.compute-1.amazonaws.com:5432/d7n2mhrn7q0jr"
